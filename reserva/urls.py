bf9d3cfc686eb9af01f0c8f1e4ab2458db19020e
Patricia Carvajal Duoc <pa.carvajall@duocuc.cl>
2022-05-14 02:21:27 -0400

    cambios 2

--- reserva/urls.py
+++ reserva/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home
 
 urlpatterns = [
-    
-    path('', home, name="home")
+    path('', home, name="home"),
+    #se deja el '' vacío, porque esto va a ser lo primero que va a mostrar
+    #el segundo parámetro es el views al que le hace referencia 
 ]

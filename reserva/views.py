a4419ec531e2d62b58d1065ce8438770e9185f6c
Isabella <isabella.moreno.bc@gmail.com>
2022-05-17 04:44:56 -0400

    crud meds sin eliminar y 1ra parte logica reserva

--- reserva/views.py
+++ reserva/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-from .forms import MedicamentoForm
+from .forms import *
 # Aquí es donde declaramos nuestras vistas personalizadas, a partir de los html que tenemos en la carpeta templates
 #en el codigo de abajo solo se pone reserva porque la función busca automáticamente la carpeta templates
 def home(request):
@@ -22,32 +22,51 @@ def crear_medicamento(request):
     }
     
     if request.method == 'POST':
-        formulario = MedicamentoForm(data=request.POST)
-        if formulario.is_valid():
-            formulario.save()
+        form = MedicamentoForm(data=request.POST)
+        if form.is_valid():
+            form.save()
             data["mensaje"] = "Guardado exitosamente"
         else:
-            data["form"] = Medicamento
+            data["form"] = form
     
     return render(request, 'reserva/agregarMedicamentos.html', data)
     
 
 def modificar_medicamento(request, codigo):
-    medicamento = get_object_or_404(Medicamento, codigo=codigo)
-    
+    meds = Medicamento.objects.get(codigo=codigo)
+    antiguoStock = meds.stock
     data = {
-        'form': MedicamentoForm(instance=medicamento)
-    }
-
+        'form': MedicamentoForm(instance=meds)
+    } 
+    
     if request.method == 'POST':
-        formulario = MedicamentoForm(data=request.POST)
-        if formulario.is_valid():
-            formulario.save()
-            data["mensaje"] = "Modificado exitosamente"
+        form = MedicamentoForm(data=request.POST, instance=meds)
+        if form.is_valid():
+            print("válido")
+            #aqui tiene que ir la funcion que valide el stock y mande las alertas.
+            nuevoStock = request.POST["stock"]
+            print(nuevoStock)
+            print(antiguoStock)
+            #esto es para probar que detecta los cambios de stock
+            if int(nuevoStock)>int(antiguoStock):
+                print("Se aumentó el stock")
+            else:
+                print("Se disminuyó el stock")
+            #     reservados = 0
+            #     for i in Reserva(idReserva):
+            #         if Reserva.objects.get(codMed=codigo):
+            #             print(Reserva(idReserva))
+            #             reservados = reservados + Reserva(cantidadReservada)
+            #             #aca faltaría capturar en una lista los telefonos y mails de las personas que tienen reserva
+            #     print(reservados)
+            #     #una vez terminado ese for, debería comparar si el stock nuevo alcanza a suplir las reservas, si pasa dentro del if, se mandan los mensajes
+            #     #y se borran las filas en Reserva
+            form.save()
             return redirect(to="gestion_medicamentos")
-        data["form"] = formulario
+        else:
+            data["form"] = form
 
-    return render(request, 'reserva/modificarMedicamento.html')
+    return render(request, 'reserva/modificarMedicamento.html', data)
 
 def receta(request):
     meds = Medicamento.objects.all()
@@ -67,3 +86,4 @@ def crear_receta(request):
 def finalizar_receta(request):
     context = {}
     return render(request, 'reserva/finalizar_receta.html', context) 
+

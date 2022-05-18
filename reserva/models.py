feb2e5841695d43056c8c953a515b8c7365b99e7
Isabella <isabella.moreno.bc@gmail.com>
2022-05-18 00:13:01 -0400

    tareas pendientes readme

--- reserva/models.py
+++ reserva/models.py
@@ -24,7 +24,6 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombreCat
 
-    
 class Medicamento(models.Model): #esta tabla guarda la información relacionada al medicamento
     codigo = models.AutoField(primary_key=True, verbose_name="Código del Medicamento") #codigo asignado de acuerdo a control interno, es clave primaria
     nombreMed = models.CharField(max_length=25, verbose_name="Nombre del Medicamento") #es el nombre comercial del medicamento
@@ -86,7 +85,6 @@ class DetalleReceta(models.Model):
     def __str__(self):
         detalle = "receta_n" + str(self.idReceta)+"medicamento_"+str(self.codmed)
         return detalle
-
 
 class Reserva(models.Model):
     idReserva = models.AutoField(primary_key=True)
